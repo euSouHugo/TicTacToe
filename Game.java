402ca0d5cec29a0c6ce3eeeb9bbc3f9664d13600
Hugo Andrade <xxhugospg10@gmail.com>
2023-12-08 04:07:22 -0300

    Add gameloop + features
    Added a prototype of the gameloop, alongside with a function that marks the gameboard.

--- Game.java
+++ Game.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
 public class Game {
 // versão 0.1
 
     public static void main(String[] args) {
-        char[][] tabuleiro = {{' ', '|', ' ', '|', ' ' },
+        char[][] tabuleiro = {
+                {' ', '|', ' ', '|', ' ' },
                 {'-', '+', '-', '+', '-' },
                 {' ', '|', ' ', '|', ' ' },
                 {'-', '+', '-', '+', '-' },
-                {' ', '|', ' ', '|', ' ' }};
+                {' ', '|', ' ', '|', ' ' }
+        };
 
+        imprimirTabuleiro(tabuleiro);
+
+        // loop do jogo (ainda sem o loop em si):
+
+        System.out.print("Digite uma posição: ");
+        Scanner scanner = new Scanner(System.in);
+        int pos = scanner.nextInt();
+        botarMarca(tabuleiro, pos);
         imprimirTabuleiro(tabuleiro);
     }
     public static void imprimirTabuleiro(char[][] tabuleiro){
@@ -16,6 +27,34 @@ public class Game {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+    public static void botarMarca(char[][] tabuleiro, int pos){
+        switch (pos){
+            case 1:
+                tabuleiro [0][0] = 'X';
+                break;
+            case 2:
+                tabuleiro [0][2] = 'X';
+                break;
+            case 3:
+                tabuleiro [0][4] = 'X';
+            case 4:
+                tabuleiro [2][0] = 'X';
+                break;
+            case 5:
+                tabuleiro [2][2] = 'X';
+                break;
+            case 6:
+                tabuleiro [2][4] = 'X';
+            case 7:
+                tabuleiro [4][0] = 'X';
+                break;
+            case 8:
+                tabuleiro [4][2] = 'X';
+                break;
+            case 9:
+                tabuleiro [4][4] = 'X';
         }
     }
 }
